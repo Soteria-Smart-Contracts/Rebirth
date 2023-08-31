089af5a0accf492466793d4fcd58825b6d2505c3
Darius Tanz <scratchticketseb@gmail.com>
2023-08-31 15:59:08 +0000

    Aug 31, 2023, 11:59 AM

--- Smart Contracts/RebirthNFT.sol
+++ Smart Contracts/RebirthNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Standard is ERC721Enumerable, Ownable {
+contract RebirthNFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string public baseURI;
