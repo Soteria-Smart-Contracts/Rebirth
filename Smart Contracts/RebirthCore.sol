a46fc91c0f226967856a4a8ad80ddf6518904e97
Darius Tanz <scratchticketseb@gmail.com>
2023-08-31 16:10:41 +0000

    Aug 31, 2023, 12:10 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -7,7 +7,7 @@ contract RebirthCore{
     ERC20 SRBH; //TODO: Change to ERC20
     ERC20 RelaunchShares;
     uint256[] public OpenPools;
-    uint256 
+    uint256[] 
 
     //Struct-Enum Declarations
 
