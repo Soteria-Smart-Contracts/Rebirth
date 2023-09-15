79090ea641b2c4a698cd03176f2953211d2300d1
Darius Tanz <scratchticketseb@gmail.com>
2023-09-15 21:46:59 +0000

    Sep 15, 2023, 5:46 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -24,7 +24,7 @@ contract RebirthCore{
     mapping(uint256 => RebirthPool) public Pools;
     mapping(uint256 => mapping(address => uint256)) public PoolDeposits;
     mapping(uint256 => uint256) OpenPoolsIndexer;
-    mapping(address => uint2256) NFT
+    mapping(address => uint2256) NFT_Gift
 
 
     //Event Declarations
