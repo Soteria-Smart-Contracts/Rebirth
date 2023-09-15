7e6d545816f69b869c2eec3e6a32eaae4f35c0ee
Darius Tanz <scratchticketseb@gmail.com>
2023-09-15 21:51:11 +0000

    Sep 15, 2023, 5:51 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.19;
 contract RebirthCore{
     //Variable Declarations
     address SRBH_Admin; //TODO: Preset?
-    ERC20 SRBH; //TODO: Change to ERC20
-    address FreemintContract;
+    ERC20 SRBH; //
+    address FreemintContract; //TODO: Set up in alternative payouts
     uint256[] public OpenPools;
     //Struct-Enum Declarations
 
