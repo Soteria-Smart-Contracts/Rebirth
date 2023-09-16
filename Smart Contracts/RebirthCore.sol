25ad9aefb2dd3dfbbb0561e345d27be248b11921
Darius Tanz <scratchticketseb@gmail.com>
2023-09-16 21:24:44 +0000

    Sep 16, 2023, 5:24 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 //Copilot, whats left to finish this contract is the following:
-//
+//A function that allows users to deposit t
 
 contract RebirthProtocolCore{
     //Variable Declarations
