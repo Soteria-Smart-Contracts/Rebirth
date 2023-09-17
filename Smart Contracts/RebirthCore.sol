4362acab94d0c2517351398e4eda27e7e05c5832
Darius Tanz <scratchticketseb@gmail.com>
2023-09-17 15:26:54 +0000

    Sep 17, 2023, 11:26 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -125,7 +125,7 @@ contract RebirthProtocolCore{
         Path[0] = UniswapRouter.WETH();
         Path[1] = address(RBH);
 
-        UniswapRouter.swapExactETHForTokens{value: address(this).balance}(0, Path, address(this), block.timestamp);
+        RBHUniswapRouter.swapExactETHForTokens{value: address(this).balance}(0, Path, address(this), block.timestamp);
 
         //Create new ERC20 token with the name and symbol of the old memecoin
         uint256 BalanceToLiquidity = Token.balanceOf(address(this));
