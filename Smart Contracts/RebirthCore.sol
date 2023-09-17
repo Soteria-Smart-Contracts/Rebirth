10458f3013c55c94ee645eda38ab8629812942cc
Darius Tanz <scratchticketseb@gmail.com>
2023-09-17 15:13:27 +0000

    Sep 17, 2023, 11:13 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -127,7 +127,7 @@ contract RebirthProtocolCore{
         UniswapRouter.swapExactETHForTokens{value: address(this).balance}(0, Path, address(this), block.timestamp);
 
         //Create new ERC20 token with the name and symbol of the old memecoin
-        
+
 
 
         //Create new RBH/Memecoin pair on uniswap, send the liquidity tokens to the zero address
@@ -171,6 +171,93 @@ contract RebirthProtocolCore{
 
 
 //TODO: Update interfaces depending on existing contracts
+
+//SPDX-License-Identifier:UNLICENSE
+pragma solidity ^0.8.20;
+
+
+contract AntonioCoin {
+    uint256 public tokenCap;
+    uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    address private ZeroAddress;
+    //variable Declarations
+    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);    
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event BurnEvent(address indexed burner, uint256 indexed buramount);
+    event ManageMinterEvent(address indexed newminter);
+    //Event Declarations 
+    
+    mapping(address => uint256) public balances;
+
+    mapping(address => mapping (address => uint256)) public allowance;
+    
+    constructor(uint256 _TokenCap, string memory _name, string memory _symbol){
+        tokenCap = _TokenCap;
+        totalSupply = 0;
+        name = _name;
+        symbol = _symbol;
+        decimals = 18;
+        Mint(msg.sender, _TokenCap);
+    }
+    
+    function balanceOf(address Address) public view returns (uint256 balance){
+        return balances[Address];
+    }
+
+    function approve(address delegate, uint _amount) public returns (bool) {
+        allowance[msg.sender][delegate] = _amount;
+        emit Approval(msg.sender, delegate, _amount);
+        return true;
+    }
+    //Approves an address to spend your coins
+
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
+        require(_amount <= balances[_from]);    
+        require(_amount <= allowance[_from][msg.sender]);
+    
+        balances[_from] = balances[_from]-(_amount);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender]-(_amount);
+        balances[_to] = balances[_to]+(_amount);
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+    //Transfer From an other address
+
+
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+        require(_amount <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender]-(_amount);
+        balances[_to] = balances[_to]+(_amount);
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+
+    function Mint(address _MintTo, uint256 _MintAmount) internal {
+        require (totalSupply+(_MintAmount) <= tokenCap);
+        balances[_MintTo] = balances[_MintTo]+(_MintAmount);
+        totalSupply = totalSupply+(_MintAmount);
+        ZeroAddress = 0x0000000000000000000000000000000000000000;
+        emit Transfer(ZeroAddress ,_MintTo, _MintAmount);
+    } //Can only be used on deploy, view Internal 
+
+
+    function Burn(uint256 _BurnAmount) public {
+        require (balances[msg.sender] >= _BurnAmount);
+        balances[msg.sender] = balances[msg.sender]-(_BurnAmount);
+        totalSupply = totalSupply-(_BurnAmount);
+        ZeroAddress = 0x0000000000000000000000000000000000000000;
+        emit Transfer(msg.sender, ZeroAddress, _BurnAmount);
+        emit BurnEvent(msg.sender, _BurnAmount);
+        
+    }
+
+}
 
 interface ERC20 {
   function balanceOf(address owner) external view returns (uint256);
