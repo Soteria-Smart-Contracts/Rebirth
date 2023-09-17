b1ab7ed38c5d3bb4d679cba9466be060b6efb4d1
Darius Tanz <scratchticketseb@gmail.com>
2023-09-17 15:52:05 +0000

    Sep 17, 2023, 11:52 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -115,7 +115,7 @@ contract RebirthProtocolCore{
         uint256 MemecoinsPerRelaunchShare = 0; //TODO: Calculate this
         
 
-        UniswapRouter.getAmountsOut()
+        UniswapRouter.getAmountsOut(0.001 ether, address(RBH), Pools[PoolID].TokenAddress);
 
 
         if (Pools[PoolID].TotalTokensDeposited < Pools[PoolID].SoftCap){
