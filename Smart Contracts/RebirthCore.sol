ee039b927c06f163c2abee2d09575bf8d1b6d1e9
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 14:11:19 +0000

    Sep 26, 2023, 10:11 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -268,7 +268,7 @@ contract RebirthProtocolCore{
     }
 }
 
-//This next contract needs to be able to accept any memecoin with any ethereum liquidity on uniswap, sell the ether, send to the rebirthcore superadmin address, then allow the user to claim once again 
+//This next contract needs to be able to accept any memecoin with any ethereum liquidity on uniswap, sell the ether, send to the rebirthcore superadmin address, then allow the user to claim once again one of the 3 available options
 contract RebirthLiquidator{
 
 }
