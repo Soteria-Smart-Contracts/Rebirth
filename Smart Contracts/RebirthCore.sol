0ef0075640b1a6f9a1ecf09a9b92b72fc7ed3b90
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 15:30:48 +0000

    Sep 26, 2023, 11:30 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -297,7 +297,7 @@ contract RebirthLiquidator {
 
     constructor(address rebirthCoreAddress) {
         RebirthCoreAddress = rebirthCoreAddress;
-        RBH_SuperAdmin = msg.sender;
+        RBH_SuperAdmin = RebirthCore;
         uniswapRouter = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     }
 
