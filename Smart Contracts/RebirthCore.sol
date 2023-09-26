6334ad305c522bc0cef132e87e6c933a47832e09
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 16:16:29 +0000

    Sep 26, 2023, 12:16 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 contract RebirthTestDeployer{
-    //this deployer needs to deploy in the constructor an RBH token and send the tokens to the deployer, a memecoin for testing, 
+    //this deployer needs to deploy in the constructor an RBH token and send half the tokens to core after deploying it aswell, and half to the deployer, a memecoin for testing, 
 }
 
 contract RebirthProtocolCore{
