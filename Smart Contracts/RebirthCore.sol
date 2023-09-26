b8a14f558e6bd0ebc02c43012cfd658186bfbfe7
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 15:38:02 +0000

    Sep 26, 2023, 11:38 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -319,8 +319,7 @@ contract RebirthLiquidator {
 
         //handle payout choice
         if(PayoutChoice == AlternativePayoutOption.RBHTokens){
-            //In this case, calculate the total RBH payout but then set it to a lock  of 10 days for the user to await before being able to claim
-
+            //In this case, calculate the total RBH payout but then set it to a lock  of 10 days for the user to await before being able to claim, dont forget to set the path to 
         }
         else if(PayoutChoice == AlternativePayoutOption.NFTFreemints){
             RebirthProtocolCore(RebirthCoreAddress).AddFreemint(msg.sender, amount / 10);
