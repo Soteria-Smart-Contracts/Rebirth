ab54331b9f939ce3ed43d631e103549a42c3091e
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 15:28:25 +0000

    Sep 26, 2023, 11:28 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -224,16 +224,12 @@ contract RebirthProtocolCore{
         Liquidator = _Liquidator;
     }
 
-    function SetFreemint(address User, uint256 Amount) public onlySuperAdmin {
+    function AddFreemint(address User, uint256 Amount) public  {
         NFT_Freemints[User] += Amount;
     }
 
-    function SetRelaunchShare(address User, uint256 Amount) public onlySuperAdmin {
+    function AddRelaunchShare(address User, uint256 Amount) public onlySuperAdmin {
         RelaunchShares[User] += Amount;
-    }
-
-    function WithdrawRBH(uint256 Amount) public onlySuperAdmin {
-        RBH.transfer(RBH_SuperAdmin, Amount);
     }
 
     function WithdrawRBH() public onlySuperAdmin {
