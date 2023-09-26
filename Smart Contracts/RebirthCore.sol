8ab1ac02940059cc84c73577397fe9ab0f05025f
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 15:43:32 +0000

    Sep 26, 2023, 11:43 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -295,7 +295,7 @@ contract RebirthLiquidator {
     IUniswapV2Router02 public uniswapRouter; 
     ERC20 public RBH;
 
-    mapping(address => mapping(address => UserRBHLiquidation)) public UserRBHLiquidation;
+    mapping(address => mapping(address => UserRBHLiquidation)) public UserRBHLiquidations;
 
     struct UserRBHLiquidation{
         uint256 RBHPayout;
