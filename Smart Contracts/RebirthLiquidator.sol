8b3fd43f1e7a7cd386334e08e164185714f5223a
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 20:28:15 +0000

    Sep 26, 2023, 4:28 PM

--- Smart Contracts/RebirthLiquidator.sol
+++ Smart Contracts/RebirthLiquidator.sol
@@ -389,7 +389,7 @@ contract RebirthLiquidator {
         }
         else if(PayoutChoice == AlternativePayoutOption.NFTFreemints){
             //TODO: Calculate the amount of freemints to give the user (1 freemint per 0.01 ether) using wETHIn
-            
+            RebirthProtocolCore(payable(RebirthCoreAddress)).AddFreemint(msg.sender, amount / 100);
         }
         else if(PayoutChoice == AlternativePayoutOption.RelaunchShares){
             //TODO: Calculate the amount of relaunch shares to give the user (1 relaunch share per 0.001 ether)
