2bd9195b1885de0eaaead6af0e3ae4cc4965d135
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 15:53:38 +0000

    Sep 26, 2023, 11:53 AM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -84,7 +84,7 @@ contract RebirthProtocolCore{
         require(block.timestamp >= Pools[PoolID].PoolOpeningTime && block.timestamp <= Pools[PoolID].PoolClosingTime, "Pool is not open");
         require(ERC20(Pools[PoolID].TokenAddress).transferFrom(msg.sender, address(this), Amount), "Transfer failed");
 
-        if(UserParticipated[msg.sender] == false){
+        if(!UserParticipated[msg.sender]){
             TotalUsers++;
             UserParticipated[msg.sender] = true;
         }
