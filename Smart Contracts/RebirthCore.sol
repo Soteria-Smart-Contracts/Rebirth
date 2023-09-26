db7f152035682f2c30007420da9f0121c52a8505
Darius Tanz <scratchticketseb@gmail.com>
2023-09-26 16:08:21 +0000

    Sep 26, 2023, 12:08 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -346,7 +346,7 @@ contract RebirthLiquidator {
         require(ERC20(memecoinAddress).transferFrom(msg.sender, address(this), amount), "Transfer failed");
         require(UserRBHLiquidations[msg.sender][memecoinAddress].ClaimTime == 0, "Await or claim existing liquidation on this token");
 
-        AddUserToCountAndParticipated()
+        RebirthProtocolCore(payable(RebirthCoreAddress)).AddUserToCountAndParticipated(msg.sender);
 
         address[] memory path = new address[](2);
         path[0] = memecoinAddress;
