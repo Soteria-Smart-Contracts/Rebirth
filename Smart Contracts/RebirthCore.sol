a38a0334f1df2fe952ee8fcc9732c668247f0c1f
Darius Tanz <scratchticketseb@gmail.com>
2023-10-08 19:44:59 +0000

    Oct 8, 2023, 3:44 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -395,7 +395,7 @@ contract RebirthLiquidator {
 
             uint256 RBH_TradeAmount = uniswapRouter.getAmountsOut(wETHIn, path)[1];
             UserRBHLiquidations[msg.sender][memecoinAddress].RBHPayout = (RBH_TradeAmount * 110) / 100;
-            UserRBHLiquidations[msg.sender][memecoinAddress].ClaimTime = block.timestamp + 864000;
+            UserRBHLiquidations[msg.sender][memecoinAddress].ClaimTime = block.timestamp + 604800;
             AllUserLiquidations[msg.sender].push(memecoinAddress);
         }
         else if(PayoutChoice == AlternativePayoutOption.NFTFreemints){
