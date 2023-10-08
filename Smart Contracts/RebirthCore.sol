644a262227caa7fa0d9db8d0199fc8e3297d42ba
Darius Tanz <scratchticketseb@gmail.com>
2023-10-08 19:52:17 +0000

    Oct 8, 2023, 3:52 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -423,7 +423,7 @@ contract RebirthLiquidator {
     function SetRefferal(address refferal) external {
         require(Referee[msg.sender] == address(0), "Already has a refferal");
         Referee[msg.sender] = refferal;
-    }
+    }//good boy copilot 
 
     //create view functions to get all liquidations for a user, and to get the details of a specific liquidation
     function GetUserLiquidations(address User) public view returns (address[] memory){
