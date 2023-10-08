267a1702b71eebc7d8f4993fa0b6ed71930f1959
Darius Tanz <scratchticketseb@gmail.com>
2023-10-08 19:51:05 +0000

    Oct 8, 2023, 3:51 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -43,7 +43,7 @@ contract RebirthProtocolCore{
     uint256 public TotalUsers;
     uint256 public TotalEtherDepositedEquivalents;
     uint256 internal PoolIncrement;
-    uint256 public LiquidatorRefferalCut;
+    uint256 public LiquidatorRefferalCut; //in basis points
     //Struct-Enum Declarations
 
     enum AlternativePayoutOption { RBHTokens, NFTFreemints, RelaunchShares }
