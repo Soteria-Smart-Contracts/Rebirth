57784b201daceb203cb82e0b0a5a1301f4858739
Darius Tanz <scratchticketseb@gmail.com>
2023-10-09 17:06:35 +0000

    Oct 9, 2023, 1:06 PM

--- Smart Contracts/RebirthCore.sol
+++ Smart Contracts/RebirthCore.sol
@@ -376,7 +376,7 @@ contract RebirthLiquidator {
 
     //create a liquidatemultiple function that allows users to liquidate multiple memecoins at once (by calling the liquidate function multiple times)
     function LiquidateMultiple(address[] memory memecoins, uint256[] memory amounts, AlternativePayoutOption memory PayoutChoice) external {
-        require(memecoins.length == amounts.length && memecoins.length == PayoutChoices.length, "Arrays must be the same length");
+        require(memecoins.length == amounts.length, "Arrays must be the same length");
         for(uint256 i = 0; i < memecoins.length; i++){
             Liquidate(memecoins[i], amounts[i], PayoutChoices[i]);
         }
